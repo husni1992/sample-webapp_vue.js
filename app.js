e4f148a521dd82ab9d4ec2954a9bdc44243bcd1c
husny_ahamed <husny.ahamed@workflowsapp.com>
2017-05-21 16:42:53 +0530

    Show total stocks count and total sports car count. Disable "Add car" button if input text is empty.

--- app.js
+++ app.js
@@ -5,13 +5,15 @@
         data: {
             title: 'Cars available in stock',
             content: 'Cars are human friends. <strong>Loved by people</strong> a lot than other mode of transport',
+            inputText: '',
             cars: [
                 { name: 'Bugati', speed: 387, count: 4 },
                 { name: 'Lambogini', speed: 290, count: 7 },
                 { name: 'Ferari', speed: 297, count: 12 },
                 { name: 'Volks Wagon', speed: 285, count: 2 }
             ],
-            inputText: ''
+            stockUpdated: 0,
+            sportsCarCountUpdated: 0
         },
         filters: {
             capitalize: function (value) {
@@ -49,18 +51,13 @@
         },
         methods: {
             addItem: function () {
-
-                // this.items.push({text: name})
-                var input = document.getElementById('itemForm');
-
-                var elem = input.value.split(',');
-
+                var elem = this.inputText.split(',');
                 var name = elem[0].replace(/\s/g, '');
                 var speed = elem[1] ? elem[1].toNumber() : 0;      // Number(elem[1]); 
 
                 if (isNaN(speed)) {
                     alert('Invalid number for speed');
-                    input.value = "";
+                    this.inputText = "";
                     return;
                 }
 
@@ -70,16 +67,24 @@
                         speed: speed,
                         count: 0
                     })
-                    input.value = "";
-                    // input.focus();
+                    this.inputText = "";
                 }
             },
             removeItem: function (index) {
                 this.cars.splice(index, 1);
             }
         },
+        watch: {
+
+        },
         computed: {
+            buttonDisabled: function () {
+                if (!this.inputText) {
+                    return true;
+                }
+            },
             stockCount: function () {
+                this.stockUpdated++;
                 var count = 0;
                 var items = this.cars;
 
@@ -94,8 +99,11 @@
                 var items = this.cars;
 
                 items.forEach(function (item) {
-                    count += item.speed > 350
-                })
+                    if (item.speed > 350) {
+                        count++;
+                        this.sportsCarCountUpdated++;
+                    }
+                }, this)
 
                 return count;
             }
