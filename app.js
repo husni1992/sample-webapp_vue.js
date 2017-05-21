59326a8e9cfdbd327a4ab1f7474fc4c6688acc90
husny_ahamed <husny.ahamed@workflowsapp.com>
2017-05-21 15:21:13 +0530

    Used component life cycle "mounted" to add a one time function, which is a prototype function to String class

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
             title: 'Cars available in stock',
             content: 'Cars are human friends. <strong>Loved by people</strong> a lot than other mode of transport',
             cars: [
-                { name: 'Bugati', speed: 320, count: 4 },
+                { name: 'Bugati', speed: 387, count: 4 },
                 { name: 'Lambogini', speed: 290, count: 7 },
                 { name: 'Ferari', speed: 297, count: 12 },
                 { name: 'Volks Wagon', speed: 285, count: 2 }
@@ -29,14 +29,46 @@
                 return "https://en.wikipedia.ORG/wifi/" + value;
             }
         },
+        mounted() {
+            // convert to number prototype method for String
+            String.prototype.toNumber = function () {
+                debugger
+                var strArray = this;
+                var error = false;
+
+                for (var prop in strArray) {
+                    if (prop != 'toNumber' && isNaN(prop)) {
+                        error = true;
+                    }
+                }
+
+                if (error) return;
+
+                return Number(this);
+            };
+        },
         methods: {
             addItem: function () {
+
                 // this.items.push({text: name})
                 var input = document.getElementById('itemForm');
 
-                if (!!input.value) {
+                var elem = input.value.split(',');
+
+                var name = elem[0].replace(/\s/g, '');
+                var speed = elem[1] ? elem[1].toNumber() : 0;      // Number(elem[1]); 
+
+                if (isNaN(speed)) {
+                    alert('Invalid number for speed');
+                    input.value = "";
+                    return;
+                }
+
+                if (!!name) {
                     this.cars.push({
-                        name: input.value
+                        name: name,
+                        speed: speed,
+                        count: 0
                     })
                     input.value = "";
                     // input.focus();
@@ -47,15 +79,25 @@
             }
         },
         computed: {
-            stockCount: function(){
-                var total = 0;
+            stockCount: function () {
+                var count = 0;
                 var items = this.cars;
 
-                items.forEach(function(item) {
-                    total += item.count && item.count || 0;
-                }, this);
+                for (var i in items) {
+                    count += items[i].count && items[i].count || 0;
+                }
 
-                return total;
+                return count;
+            },
+            sportsCarsCount: function () {
+                var count = 0;
+                var items = this.cars;
+
+                items.forEach(function (item) {
+                    count += item.speed > 350
+                })
+
+                return count;
             }
         }
     })
